dcc0c90e4de4035785baeb1761c89984d66c0174
heelpeeri <164684516+heelpeeri@users.noreply.github.com>
2026-04-24 19:10:48 +0300

    Update quiz.ts

--- src/data/quiz.ts
+++ src/data/quiz.ts
@@ -876,7 +876,7 @@ export const quizQuestions: Record<QuizCategoryKey, QuizQuestion[]> = {
     },
     {
     question: "ولد عم سعيدان وعليان اللي جاي من الديرة ما يحب الحمام، اجل وش يحب؟",
-    image: "/images/tash-fish.jpg",
+    image: "/images/tash-fish.jgp.png",
     answer: "السمك",
     },
     {
