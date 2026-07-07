356d26f71717c20e3c4560db4241ab0275bfcd09
heelpeeri <164684516+heelpeeri@users.noreply.github.com>
2026-07-07 21:30:01 +0300

    Update scramble.ts

--- src/data/scramble.ts
+++ src/data/scramble.ts
@@ -1,37 +1,47 @@
-export const WHO_GAME = [ 
-{ 
-id: "hasson", 
-image: "/images/people/hasson.png", 
-answer: "حسون البارقي", 
-}, 
-{ 
-id: "small", 
-image: "/images/people/small.png", 
-answer: "سمول كبتشينو", 
-}, 
-{ 
-id: "cr7", 
-image: "/images/people/CR7.png", 
-answer: "كريستيانو رونالدو", 
-}, 
-{ 
-id: "riyadh", 
-image: "/images/people/riyadh.png", 
-answer: "أسطورة الرياض", 
-}, 
-{ 
-id: "foad", 
-image: "/images/people/foad.png", 
-answer: "فؤاد (طاش)", 
-}, 
-{ 
-id: "trump", 
-image: "/images/people/trump.png", 
-answer: "ترامب", 
-}, 
-{ 
-id: "amer", 
-image: "/images/people/amer.png", 
-answer: "عامر (شباب البومب)", 
-}, 
+export const WHO_GAME = [
+  {
+    id: "hasson",
+    image: "/images/people/hasson.png",
+    answer: "حسون البارقي",
+  },
+  {
+    id: "small",
+    image: "/images/people/small.png",
+    answer: "سمول كبتشينو",
+  },
+  {
+    id: "CR7",
+    image: "/images/people/CR7.png",
+    answer: "كريستيانو رونالدو",
+  },
+  {
+    id: "foad",
+    image: "/images/people/foad.png",
+    answer: "فؤاد (طاش)",
+  },
+  {
+    id: "trump",
+    image: "/images/people/trump.png",
+    answer: "ترامب",
+  },
+  {
+    id: "amer",
+    image: "/images/people/amer.png",
+    answer: "عامر (شباب البومب)",
+  },
+  {
+    id: "riyadh",
+    image: "/images/people/riyadh.png",
+    answer: "أسطورة الرياض",
+  },
+  {
+    id: "you",
+    image: "/images/people/you.png",
+    answer: "اوصف نفسك شف يعرفونك ولا",
+  },
+  {
+    id: "rbeah",
+    image: "/images/people/rbeah.png",
+    answer: "ربيعة",
+  },
 ];
